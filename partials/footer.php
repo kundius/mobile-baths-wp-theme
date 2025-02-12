ad9f247618d2b568e9136e0a97a13caffe804dc2
Kundius Ruslan <kundius.ruslan@gmail.com>
2025-02-12 17:55:14 +0300

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -8,7 +8,7 @@
           <?php echo carbon_get_theme_option('crb_theme_phone') ?>
         </a>
         <meta itemprop="openingHours" content="Пн - Сб : 09.00 - 20.00, Вс - выходной, без перерывов">
-        <a itemprop="email" href="mail:<?php echo carbon_get_theme_option('crb_theme_email') ?>" class="sticky-footer__email">
+        <a itemprop="email" href="mailto:<?php echo carbon_get_theme_option('crb_theme_email') ?>" class="sticky-footer__email">
           <span><?php icon('mail', 12) ?></span>
           <?php echo carbon_get_theme_option('crb_theme_email') ?>
         </a>
